5973c2ba460dfbf23658acd56faab3d26493fc8a
Javier Catalá <javicatala@iesdoctorbalmis.com>
2020-01-17 00:41:14 +0100

    Agregar archivos de proyecto.

---
+++ SimpleMVVM/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMVVM
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public Persona Usuario { get; set; }
+
+        public MainWindowVM()
+        {
+            Usuario = new Persona();
+        }
+
+        public void Limpiar()
+        {
+            Usuario = new Persona();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
---
+++ SimpleMVVM/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMVVM
+{
+    class Persona : INotifyPropertyChanged
+    {
+        public double Altura { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Imc
+        {
+            get { return (Altura == 0) ? 0 : Peso / Altura; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
